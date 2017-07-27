9c7ef4c72ff738926437fd076482d9c60f7b81b9
亭 <353326403@qq.com>
2017-07-27 14:39:03 +0800

    no message

--- src/Core/Networking/NetworkConfig.js
+++ src/Core/Networking/NetworkConfig.js
@@ -5,3 +5,29 @@ export const baseURL = "http://godevelopments.co.uk"
  // export const baseURL = "192.168.0.143:8112"
 // export const baseURL = "http://maskhao.cn:8080"
 export const timeOut = 50000
+
+export const RPC_TIMEOUT = 3600000; // default rpc timeout: one hour
+
+//华东访问地址
+export const EAST_CHINA_HTTP_DOWNLOAD = 'http://iovip.qbox.me';
+export const EAST_CHINA_HTTPS_DOWNLOAD = 'https://iovip.qbox.me';
+export const EAST_CHINA_HTTP_UPLOAD_SERVER = 'http://up.qiniu.com';//服务端
+export const EAST_CHINA_HTTP_UPLOAD_CLIENT = 'http://upload.qiniu.com';//客户端
+export const EAST_CHINA_HTTPS_UPLOAD_SERVER = 'https://up.qbox.me';//服务端
+export const EAST_CHINA_HTTPS_UPLOAD_CLIENT = 'https://upload.qbox.me';//客户端
+
+//华北访问地址
+export const NORTH_CHINA_HTTP_DOWNLOAD = 'http://iovip-z1.qbox.me';
+export const NORTH_CHINA_HTTPS_DOWNLOAD = 'https://iovip-z1.qbox.me';
+export const NORTH_CHINA_HTTP_UPLOAD_SERVER = 'http://up-z1.qiniu.com';//服务端
+export const NORTH_CHINA_HTTP_UPLOAD_CLIENT = 'http://upload-z1.qiniu.com';//客户端
+export const NORTH_CHINA_HTTPS_UPLOAD_SERVER = 'https://up-z1.qbox.me';//服务端
+export const NORTH_CHINA_HTTPS_UPLOAD_CLIENT = 'https://upload-z1.qbox.me';//客户端
+
+//华南访问地址
+export const SOUTH_CHINA_HTTP_DOWNLOAD = 'http://iovip-z2.qbox.me';
+export const SOUTH_CHINA_HTTPS_DOWNLOAD = 'https://iovip-z2.qbox.me';
+export const SOUTH_CHINA_HTTP_UPLOAD_SERVER = 'http://up-z2.qiniu.com';//服务端
+export const SOUTH_CHINA_HTTP_UPLOAD_CLIENT = 'http://upload-z2.qiniu.com';//客户端
+export const SOUTH_CHINA_HTTPS_UPLOAD_SERVER = 'https://up-z2.qbox.me';//服务端
+export const SOUTH_CHINA_HTTPS_UPLOAD_CLIENT = 'https://upload-z2.qbox.me';//客户端
---
+++ src/Core/Networking/NetworkFileUpload.js
@@ -0,0 +1,89 @@
+function httpRequestPOST(requestURL,params,headers={},callback){
+    let requestObject= initRequestObject(headers,"POST",params,callback)
+    promiseRequest(requestURL,requestObject,callback);
+}
+import config from "./NetworkConfig"
+
+export function uploadFile(uri, token, formInput, onprogress) {
+    return new Promise((resolve, reject)=> {
+        if (typeof uri != 'string' || uri == '' || typeof formInput.key == 'undefined') {
+            reject && reject(null);
+            return;
+        }
+        if (uri[0] == '/') {
+            uri = "file://" + uri;
+        }
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', SOUTH_CHINA_HTTP_UPLOAD_CLIENT);
+        xhr.onload = () => {
+            if (xhr.status !== 200) {
+                reject && reject(xhr);
+                return;
+            }
+
+            resolve && resolve(xhr);
+        };
+
+        var formdata = new FormData();
+        formdata.append("key", formInput.key);
+        formdata.append("token", token);
+        if (typeof formInput.type == 'undefined')
+            formInput.type = 'application/octet-stream';
+        if (typeof formInput.name == 'undefined') {
+            var filePath = uri.split("/");
+            if (filePath.length > 0)
+                formInput.name = filePath[filePath.length - 1];
+            else
+                formInput.name = "";
+        }
+        formdata.append("file", {uri: uri, type: formInput.type, name: formInput.name});
+        xhr.upload.onprogress = (event) => {
+            onprogress && onprogress(event, xhr);
+        };
+        xhr.send(formdata);
+    });
+}
+
+export class ImageView {
+    constructor(mode, width, height, quality, format) {
+        this.mode = mode || 1;
+        this.width = width || 0;
+        this.height = height || 0;
+        this.quality = quality || 0;
+        this.format = format || null;
+    }
+
+    makeRequest(url) {
+        url += '?imageView2/' + this.mode;
+
+        if (this.width > 0) {
+            url += '/w/' + this.width;
+        }
+
+        if (this.height > 0) {
+            url += '/h/' + this.height;
+        }
+
+        if (this.quality > 0) {
+            url += '/q/' + this.quality;
+        }
+
+        if (this.format) {
+            url += '/format/' + this.format;
+        }
+
+        return url;
+    }
+}
+
+export class ImageInfo {
+    static makeRequest(url) {
+        return url + '?imageInfo'
+    }
+}
+
+export class Exif {
+   static makeRequest(url) {
+        return url + '?exif'
+    }
+}
