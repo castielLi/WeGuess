2dfcdd76eb835e962159519019ad7435886b0c77
亭 <353326403@qq.com>
2017-07-27 17:45:24 +0800

    no message

---
+++ src/Core/QNY/config.js
@@ -0,0 +1,55 @@
+
+let RPC_TIMEOUT = 3600000; // default rpc timeout: one hour
+let TOKEN_URL="http://www.zyyft.s1.natapp.cc/api/qiniuyun/get?key=";
+let QIY_URL ="http://otf8y2r54.bkt.clouddn.com/";
+//华东访问地址
+let EAST_CHINA_HTTP_DOWNLOAD = 'http://iovip.qbox.me';
+let EAST_CHINA_HTTPS_DOWNLOAD = 'https://iovip.qbox.me';
+let EAST_CHINA_HTTP_UPLOAD_SERVER = 'http://up.qiniu.com';//服务端
+let EAST_CHINA_HTTP_UPLOAD_CLIENT = 'http://upload.qiniu.com';//客户端
+let EAST_CHINA_HTTPS_UPLOAD_SERVER = 'https://up.qbox.me';//服务端
+let EAST_CHINA_HTTPS_UPLOAD_CLIENT = 'https://upload.qbox.me';//客户端
+
+//华北访问地址
+let NORTH_CHINA_HTTP_DOWNLOAD = 'http://iovip-z1.qbox.me';
+let NORTH_CHINA_HTTPS_DOWNLOAD = 'https://iovip-z1.qbox.me';
+let NORTH_CHINA_HTTP_UPLOAD_SERVER = 'http://up-z1.qiniu.com';//服务端
+let NORTH_CHINA_HTTP_UPLOAD_CLIENT = 'http://upload-z1.qiniu.com';//客户端
+let NORTH_CHINA_HTTPS_UPLOAD_SERVER = 'https://up-z1.qbox.me';//服务端
+let NORTH_CHINA_HTTPS_UPLOAD_CLIENT = 'https://upload-z1.qbox.me';//客户端
+
+//华南访问地址
+let SOUTH_CHINA_HTTP_DOWNLOAD = 'http://iovip-z2.qbox.me';
+let SOUTH_CHINA_HTTPS_DOWNLOAD = 'https://iovip-z2.qbox.me';
+let SOUTH_CHINA_HTTP_UPLOAD_SERVER = 'http://up-z2.qiniu.com';//服务端
+let SOUTH_CHINA_HTTP_UPLOAD_CLIENT = 'http://upload-z2.qiniu.com';//客户端
+let SOUTH_CHINA_HTTPS_UPLOAD_SERVER = 'https://up-z2.qbox.me';//服务端
+let SOUTH_CHINA_HTTPS_UPLOAD_CLIENT = 'https://upload-z2.qbox.me';//客户端
+
+export default {
+	RPC_TIMEOUT,
+	TOKEN_URL,
+    //华东访问地址
+    EAST_CHINA_HTTP_DOWNLOAD,
+    EAST_CHINA_HTTPS_DOWNLOAD,
+    EAST_CHINA_HTTP_UPLOAD_SERVER,
+    EAST_CHINA_HTTP_UPLOAD_CLIENT,
+    EAST_CHINA_HTTPS_UPLOAD_SERVER,
+    EAST_CHINA_HTTPS_UPLOAD_CLIENT,
+
+    //华北访问地址
+    NORTH_CHINA_HTTP_DOWNLOAD,
+    NORTH_CHINA_HTTPS_DOWNLOAD,
+    NORTH_CHINA_HTTP_UPLOAD_SERVER,
+    NORTH_CHINA_HTTP_UPLOAD_CLIENT,
+    NORTH_CHINA_HTTPS_UPLOAD_SERVER,
+    NORTH_CHINA_HTTPS_UPLOAD_CLIENT,
+
+    //华南访问地址
+    SOUTH_CHINA_HTTP_DOWNLOAD,
+    SOUTH_CHINA_HTTPS_DOWNLOAD,
+    SOUTH_CHINA_HTTP_UPLOAD_SERVER,
+    SOUTH_CHINA_HTTP_UPLOAD_CLIENT,
+    SOUTH_CHINA_HTTPS_UPLOAD_SERVER,
+    SOUTH_CHINA_HTTPS_UPLOAD_CLIENT
+}
--- src/Core/QNY/index.js
+++ src/Core/QNY/index.js
@@ -2,16 +2,17 @@
  * Created by apple on 2017/7/27.
  */
 
-import netWorking from '../Networking/Network'
+import Rpc from './rpc';
+import Config from './config';
+import ImgOps from './imageops';
 
-let __instance = (function () {
-    let instance;
-    return (newInstance) => {
-        if (newInstance) instance = newInstance;
-        return instance;
-    }
+let __instance = (function() {
+	let instance;
+	return(newInstance) => {
+		if(newInstance) instance = newInstance;
+		return instance;
+	}
 }());
-
 
 // var ACCESS_KEY = "";
 // var SECRET_KEY = "";
@@ -19,33 +20,63 @@ let __instance = (function () {
 let _network = new netWorking();
 
 export default class QNY {
-    constructor() {
-        if (__instance()) return __instance();
+	constructor() {
+		if(__instance()) return __instance();
 
-        __instance(this);
-        this.network = _network;
-    }
+		__instance(this);
+		this.network = _network;
+	}
 
+	getAccessKeyAndSecretKey() {
 
-    getAccessKeyAndSecretKey(){
+	}
 
+	uploadFile(fileUri, onprogress) {
+		return new Promise((resolve, reject) => {
+			let keys = fileUri.split("/");
+			let key = keys[keys.length - 1];
 
-        //this.network.methodPOST(url,content,function(){
-        //
-        //      do something
-        //  
-        // },false,{"Content-Type":"application/x-www-form-urlencoded"})
-    }
+			var that = this;
+			fetch(Config.TOKEN_URL + key).then(response => {
+				if(response.status >= 200 && response.status < 300) {
+					return response
+				} else {
+					reject && reject(response);
+				}
 
-    uploadFile(){
+			}).then(response => {
+				return response.json();
+			}).then(response => {
+				let formInput = {
+					key: key,
+					// formInput对象如何配置请参考七牛官方文档“直传文件”一节
+				}
+				Rpc.uploadFile(fileUri, response.Value, formInput, function(event, xhr) {
+					onprogress(event);
+				}).then(() => {
+					let result = {
+						key: key,
+						url: Config.QIY_URL + key
+					}
+					resolve && resolve(result);
+				}, (xhr) => {
+					reject && reject(xhr);
+				});
+			}).catch(e => {
+				reject && reject(e);
+			});
+		});
+	}
+	Thumbnail(orgImgUrl,width, height, quality, format) {
+        let imgInfo = new ImgOps.ImageView(1, width, height, quality, format);
+        let url = imgInfo.makeRequest(orgImgUrl);
+        return url;
+	}
+	downloadFile() {
 
-    }
+	}
 
-    downloadFile(){
-
-    }
-
-    // imageSyncOperation(){
-    //
-    // }
+	// imageSyncOperation(){
+	//
+	// }
 }
